fbabd78d6cbac91599bd20d8896959b0bd75c384
Rafi Ozick <rafi@ozick.com>
2018-02-05 13:18:34 +0200

    added comments to Message Controller

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -13,20 +13,35 @@ class MessageController extends Controller
      * @param  int  $id
      * @return Response
      */
+
+     /**
+      * Send message to the queue
+      * @var string data; enum message_type, category
+      */
     public function sendMessage(Request $request) {
+        
         $data = $request->input('data');
         $message_type = $request->input('message_type');
         $category = $request->input('category');
+
+        // Create new queue and add item
         $queue = new QueueClass();
         $queue->createItem($data, $message_type, $category);
         
         return response()->json($queue->numberOfItems());
     }
 
+     /**
+      * Reserve message in the queue
+      * @var string data, reserver_name; enum message_type, category
+      */
     public function reserveMessage(Request $request) {
+        
         $message_type = $request->input('message_type'); 
         $category = $request->input('category'); 
         $reserver_name = $request->input('reserver_name');
+        
+        // Reserve queue item
         $queue = new QueueClass();
         $queue->reserveItem($message_type, $category, $reserver_name);
         if (!$queue->canReserve) {
@@ -35,8 +50,12 @@ class MessageController extends Controller
         
         return response()->json($queue->reserved_item);
     }
-
+     /**
+      * Delete message in the queue
+      * @var integer item_id
+      */
     public function deleteMessage($item_id) {
+
         $queue = new QueueClass();
         $queue->deleteItem($item_id);
     }
